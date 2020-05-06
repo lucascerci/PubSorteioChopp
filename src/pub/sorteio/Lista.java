3e3062151c41642db588b8c53fb26f91160e197d
Lucas.Fernandes <lucas.fernandes001p@gmail.com>
2020-05-06 01:02:48 -0300

    finalizando trabalho, removendo numeros e adicionando mensagens

--- src/pub/sorteio/Lista.java
+++ src/pub/sorteio/Lista.java
@@ -1,97 +1,142 @@
 package pub.sorteio;
 
+import javax.swing.JOptionPane;
+
 public class Lista {
 	private No inicio;
 	private No fim;
 	private int quantidade;
 	
 	public void inserirFim(No novoNo) {
-		if(inicio==null) {
-			inicio = novoNo;
-			quantidade++;
-		} else {
-			fim.setProximo(novoNo);
-			novoNo.setAnterior(fim);
-			fim = novoNo;
-			quantidade++;
-		}
+            if(inicio==null) {
+		inicio = novoNo;
+		quantidade++;
+            } else {
+		fim.setProximo(novoNo);
+		novoNo.setAnterior(fim);
 		fim = novoNo;
+		quantidade++;
+            }
+            fim = novoNo;
 	}
+    
+	public void removerCli(int num){
+            if(inicio == null){
+                JOptionPane.showMessageDialog(null, "Lista Vazia");
+            } else {
+                No noRem = inicio;
+                int flag = 0;
+                int i = 0;
+                while(flag == 0){
+                    if(num == noRem.getNumero()){
+                        System.out.println(noRem.getNumero());
+                        System.out.println(i);
+                        removerPosEsp(i);
+                        flag = 1;
+                        JOptionPane.showMessageDialog(null, "Exclusão bem sucedida");
+                    }
+                    noRem = noRem.getProximo();
+                    i = i + 1;
+                }
+                if(flag == 0){
+                    JOptionPane.showMessageDialog(null, "Não há o número " + num +" na lista");
+                }
+            }
+        }
+        
+    public void numero(int sorteado){
+            No noSor = inicio;
+            if(inicio == null) {
+                JOptionPane.showMessageDialog(null, "Lista Vazia");
+            } else {
+		if(sorteado == 0) {
+                    JOptionPane.showMessageDialog(null,"Número sorteado: "+ noSor.numero);
+		} else if(sorteado == quantidade) {
+                    noSor = fim;
+                    JOptionPane.showMessageDialog(null,"Número sorteado: "+ noSor.numero);
+		} else {
+                    int cont = 0;
+				
+                    while(noSor.getProximo() != null && cont < sorteado) {
+                        noSor = noSor.getProximo();
+                        cont++;
+                    }
+                    JOptionPane.showMessageDialog(null,"Número sorteado: "+ noSor.numero);
+                }
+            }
+        }
 	
 	public void removerFim() {
-		if(inicio == null) {
-			System.out.println("Lista Vazia");
+            if(inicio == null) {
+                JOptionPane.showMessageDialog(null, "Lista Vazia");
+            } else {
+		No noAnt = fim.getAnterior();
+		if(noAnt == null) {
+                    inicio = null;
+                    fim = null;
+                    quantidade--;
 		} else {
-			No noAnt = fim.getAnterior();
-			if(noAnt == null) {
-				inicio = null;
-				fim = null;
-				quantidade--;
-			} else {
-				noAnt.setProximo(null);
-				fim.setAnterior(null);
-				fim = noAnt;
-				quantidade--;
-			}
+                    noAnt.setProximo(null);
+                    fim.setAnterior(null);
+                    fim = noAnt;
+                    quantidade--;
 		}
+            }
 	}
 	
 	public void removerInicio() {
-		if(inicio == null) {
-			System.out.println("Lista Vazia");
-		} else {
-			No noAux = inicio;
-			inicio = inicio.getProximo();
-			noAux.setProximo(null);
-		}
+            if(inicio == null) {
+                JOptionPane.showMessageDialog(null, "Lista Vazia");
+            } else {
+                No noAux = inicio;
+		inicio = inicio.getProximo();
+		noAux.setProximo(null);
+            }
 	}
 	
 	public void removerPosEsp(int pos) {
-		if(inicio == null) {
-			System.out.println("Lista Vazia");
+            if(inicio == null) {
+                JOptionPane.showMessageDialog(null, "Lista Vazia");
+            } else {
+                if(pos == 0) {
+                    removerInicio();
+                    quantidade--;
+                } else if(pos == quantidade) {
+                    removerFim();
 		} else {
-			if(pos == 0) {
-				removerInicio();
-				quantidade--;
-			} else if(pos == quantidade) {
-				removerFim();
-			} else {
-				No noAux = inicio;
-				int cont = 0;
-				
-				while(noAux.getProximo() != null && cont < pos) {
-					noAux = noAux.getProximo();
-					cont++;
-				}
-				
-				No noAnt = noAux.getAnterior();
-				if(noAnt != null) {
-					noAnt.setProximo(noAux.getProximo());
-				}
-				quantidade--;
-			}
+                    No noAux = inicio;
+                    int cont = 0;		
+                    while(noAux.getProximo() != null && cont < pos) {
+			noAux = noAux.getProximo();
+			cont++;
+                    }
+                    No noAnt = noAux.getAnterior();
+                    if(noAnt != null) {
+                        noAnt.setProximo(noAux.getProximo());
+                    }
+                    quantidade--;
 		}
+            }
 	}
 	
-	
 	public void listar() {
-		if(inicio == null) {
-			System.out.println("Lista Vazia");
-		} else {
-			No noAux = inicio;
+            if(inicio == null) {
+                JOptionPane.showMessageDialog(null, "Lista Vazia");
+            } else {
+		No noAux = inicio;
 			
-			while(noAux != null) {
-				System.out.println(noAux.getNumero());
-				noAux = noAux.getProximo();
-			} 
-		}
+                    while(noAux != null) {
+			System.out.println(noAux.getNumero());
+			noAux = noAux.getProximo();
+                    } 
+            }
 	}
 
 	public int getQuantidade() {
-		return quantidade;
+            return quantidade;
 	}
 
 	public void setQuantidade(int quantidade) {
-		this.quantidade = quantidade;
+            this.quantidade = quantidade;
 	}
 }
--- src/pub/sorteio/Menu.java
+++ src/pub/sorteio/Menu.java
@@ -14,31 +14,41 @@ public class Menu {
 		while(menu != 0) {
 			menu = Integer.parseInt(JOptionPane.showInputDialog("Digite 0 - Sair\n" +
 					"Digite 1 - Adicionar Cliente\n" +
-					"Digite 2 - Remover Cliente\n" + 
-					"Digite 3 - Sortear Cliente"));
+					"Digite 2 - Baixa Número Cliente\n" +
+					"Digite 3 - Sortear Cliente\n" +
+					"Digite 4 - Lista de Clientes"));
 			
 			if(menu == 1) {
 				No novoNo = new No();
 				novoNo.setNumero(Integer.parseInt(JOptionPane.showInputDialog("Digite o Numero")));
 				lista.inserirFim(novoNo);
 			} else if(menu == 2) {
-				lista.removerFim();
-				lista.listar();
+                                int num = Integer.parseInt(JOptionPane.showInputDialog("Insira o número a ser removido"));
+				lista.removerCli(num);
 			} else if(menu == 3) {
-				 Random gerador = new Random();
-				 int sorteado = gerador.nextInt(lista.getQuantidade());
-				 System.out.println("numero sorteado: " + sorteado);
-				 System.out.println("-------------------------------------");
-				 System.out.println("ANTES");
-				 System.out.println("-------------------------------------");
-				 lista.listar();
-				 System.out.println("-------------------------------------");
-				 lista.removerPosEsp(sorteado);
-				 System.out.println("DEPOIS");
-				 System.out.println("-------------------------------------");
-				 lista.listar();
-				 System.out.println("-------------------------------------");
-			}
+				Random gerador = new Random();
+				if(lista.getQuantidade() == 0){
+                                        System.out.println("Lista Vazia");
+                                } else {
+                                        int sorteado = gerador.nextInt(lista.getQuantidade());
+                               	         JOptionPane.showMessageDialog(null,"Posição Sorteada: " + sorteado);
+                                        lista.numero(sorteado);
+                                        System.out.println("-------------------------------------");
+                                        System.out.println("ANTES");
+                                        System.out.println("-------------------------------------");
+                                        lista.listar();
+                                        System.out.println("-------------------------------------");
+                                        lista.removerPosEsp(sorteado);
+                                        System.out.println("DEPOIS");
+                                        System.out.println("-------------------------------------");
+                                        lista.listar();
+                                        System.out.println("-------------------------------------");
+                                } 
+			} else if(menu == 4){
+                                lista.listar();
+                        }else {
+                                JOptionPane.showMessageDialog(null,"Opção Inválida");
+                        }
 		}
 	}
 
